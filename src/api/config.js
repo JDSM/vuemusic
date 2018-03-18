5dd90cd91a067b9db89f51807324776ad36660f5
JDSM <jesusdmalave@gmail.com>
2018-03-17 20:53:33 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+/*Application name	VueMusic*/
+/*API key	79136f749a3195dd1e75243988c9b101*/
+/*Shared secret	9f1346ca298c6a549df6f2191c23fe41*/
+/*Registered to	jdserranoma*/
+
+export default {
+	appName: 'VueMusic',
+	apiKey:'79136f749a3195dd1e75243988c9b101',
+	secret: '9f1346ca298c6a549df6f2191c23fe41',
+	registereTo:'jdserranoma'
+}
